546ffb243542305168b9029b7979341b16afff7d
suren67 <surendranpandi02@gmail.com>
2024-10-22 15:34:39 +0530

    22.10.2024_15.34

--- server/videoCreator.js
+++ server/videoCreator.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 import fetch from 'node-fetch';
 import ffmpegPath from 'ffmpeg-static';
 import { fileURLToPath } from 'url';
-import * as Jimp from 'jimp'; // Use named import for jimp
+import Jimp from 'jimp/es'; // Import Jimp from the ES module path
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
