894bd145c56d7682416ce9da643c0b595827f6d2
suren67 <surendranpandi02@gmail.com>
2024-10-22 13:08:14 +0530

    22.10.2024_13.08

--- public/script.js
+++ public/script.js
@@ -40,9 +40,9 @@ document.getElementById('reel-form').addEventListener('submit', async (event) =>
         // Display video and download link
         result.innerHTML = `
             <p>${data.message}</p>
-            <video controls src="${data.reel}" style="max-width: 10%; height: auto;"></video>
+            <video controls src="${data.reelUrl}" style="max-width: 10%; height: auto;"></video>
             <br>
-            <a href="${data.reel}" download="reel.mp4" class="download-button">Download Video</a>
+            <a href="${data.reelUrl}" download="reel.mp4" class="download-button">Download Video</a>
         `;
     } catch (error) {
         loader.style.display = 'none'; // Hide loader if there's an error
